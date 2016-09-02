81f527d31b29100060c2fc5895ca622c6ab4195f
surajshanbhag <surajmshanbhag@gmail.com>
2016-09-02 16:05:24 -0400

    start_BBB.sh :Network Routing Based on IP

--- start_BBB.sh
+++ start_BBB.sh
@@ -1,5 +1,7 @@
-interfaceName=`ifconfig | grep "80:30:dc:90:f2:eb"`
+BBBIP="192.168.7.2"
+interfaceName=`arp -a | grep $BBBIP`
 set -- $interfaceName
-sudo iptables --append FORWARD --in-interface $1 -j ACCEPT
+echo "Using interface :$7"
+sudo iptables --append FORWARD --in-interface $7 -j ACCEPT
 sudo iptables --table nat --append POSTROUTING --out-interface wlp5s0 -j MASQUERADE
 sudo sh -c "echo 1 > /proc/sys/net/ipv4/ip_forward"
