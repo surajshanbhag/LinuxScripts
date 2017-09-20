e59305e396597e8e7f33f9de49bc147846049664
Suraj M Shanbhag <surajmshanbhag@gmail.com>
2017-09-20 13:56:50 -0400

    Add files via upload

--- install_basic.sh
+++ install_basic.sh
@@ -7,8 +7,6 @@ sudo apt-get -y autoremove
 
 sudo apt-get install vim guake terminator screen tmux -y
 
-
-
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886 -y
 echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
 sudo add-apt-repository ppa:graphics-drivers/ppa -y
@@ -30,6 +28,12 @@ sudo apt-get install indicator-netspeed-unity -y
 sudo apt-get install indicator-sound-switcher -y
 sudo apt-get install spotify-client -y
 sudo apt-get install indicator-brightness -y
+sudo apt-get install git-cola
+
+wget https://atom.io/download/deb
+mv deb atom-amd64.deb
+sudo dpkg -i atom-amd64.deb
+rm -rf atom-amd64.deb
 
 cp /etc/default/grub ~/grub.bkp
 sudo sed -i -- 's/GRUB_CMDLINE_LINUX="/GRUB_CMDLINE_LINUX="acpi_backlight=native acpi_osi="/g' /etc/default/grub
@@ -73,14 +77,17 @@ cp ~/.bashrc bashrc_copy
 echo 'export PATH=/usr/local/cuda-8.0/bin${PATH:+:${PATH}}' >>~/.bashrc
 echo 'export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}' >>~/.bashrc
 
-echo "PS1='[${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u\[\033[00m\]]:\[\033[01;34m\]\w\[\033[00m\]\$ '" >> ~/.bashrc    
-echo "alias sbc='source ~/.bashrc'"
-echo "alias gits='git status'"
+echo "PS1='[${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u\[\033[00m\]]:\[\033[01;34m\]\w\[\033[00m\]\$ '" >> ~/.bashrc
+echo "alias sbc='source ~/.bashrc'">>~/.bashrc
+echo "alias gits='git status'">>~/.bashrc
 echo "alias gitp='git push origin master'"
-echo "alias catkin_make_release='catkin_make -DCMAKE_BUILD_TYPE=Release'"
-echo "alias killjobs='jobs -ps | xargs kill -9'"
+echo "alias catkin_make_release='catkin_make -DCMAKE_BUILD_TYPE=Release'">>~/.bashrc
+echo "alias killjobs='jobs -ps | xargs kill -9'">>~/.bashrc
+echo "alias sros='source ~/catkin_ws/devel/setup.bash && source /opt/ros/kinetic/setup.bash'">>~/.bashrc
 
-
+wget https://www.edubuntu.org/sites/default/files/docimages/wallpapers-oneiric/Stalking_Ocelot_by_Sayantan_Chaudhuri-full.jpg
+mv Stalking_Ocelot_by_Sayantan_Chaudhuri-full.jpg ~/Pictures/Stalking_Ocelot_by_Sayantan_Chaudhuri-full.jpg
+gsettings get org.gnome.desktop.background picture-uri ~/Pictures/Stalking_Ocelot_by_Sayantan_Chaudhuri-full.jpg
 
 #cd ~/
 #mkdir opencv
